772d8e65e9908a8f9ce31008b936a33950bcd217
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2025-08-31 12:32:18 +0530

    Updating Counter

---
+++ 01counter/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>React</h1>
+      <h2>Counter Value : 5</h2>
+      <button>Add Value</button>
+      <br />
+      <button>Decrease Value</button>
+    </>
+  );
+}
+
+export default App;
