d617de42235f0723905d799c13c3f93297b408fc
Atul Mishra <40742234+mishraatul930@users.noreply.github.com>
2025-08-31 13:01:21 +0530

    State Management

--- 01counter/src/App.jsx
+++ 01counter/src/App.jsx
@@ -1,13 +1,28 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  // let counter = 5;
+  const [counter, setCounter] = useState(5);
+
+  const addValue = () => {
+    console.log("Add Code working");
+    setCounter(counter + 1);
+    console.log(counter);
+  };
+  const decreaseValue = () => {
+    console.log("Decrease Code working");
+    setCounter(counter - 1);
+    console.log(counter);
+  };
   return (
     <>
       <h1>React</h1>
-      <h2>Counter Value : 5</h2>
-      <button>Add Value</button>
+      <h2>Counter Value : {counter}</h2>
+      <button onClick={addValue}>Add Value</button>
       <br />
-      <button>Decrease Value</button>
+      <button onClick={decreaseValue}>Decrease Value</button>
+      <p>Counter is getting updated : {counter}</p>
     </>
   );
 }
